2183fbbe5faebfe42b6c7e7f9b8a896eb36a6409
ducnna <ducnna@aht-asia.com>
2023-09-27 15:04:47 +0700

    fix

--- process.py
+++ process.py
@@ -1,71 +1,81 @@
-from selenium import webdriver
 from selenium.webdriver.remote.webdriver import By
 import selenium.webdriver.support.expected_conditions as EC  # noqa
-from time import sleep
 import undetected_chromedriver as uc
-import xlwings as xw
 from selenium.webdriver.support.ui import Select
 import datetime
+import xlwings as xw
 
 
-def process(filename, sheet):
-    if sheet == '1Thang1Lan (1T1L)' or sheet == '1ThangNhieuLan (1TNL)':
+def process(sheet, data):
+    default_data = 'DataMacDinh.xlsx'
+    stay30days = ['1Thang1Lan', '1T1L', '一月单次', '1ThangNhieuLan', '1TNL', '一月多次']
+    stay30 = False
+    for s in stay30days:
+        if s in sheet.strip():
+            stay30 = True
+            break
+    if stay30:
         stay = 30
     else:
         stay = 90
+
+    multi_entry = ['1ThangNhieuLan', '1TNL', '一月多次', '3ThangNhieuLan', '3TNL', '三月多次']
+    multi = False
+    for m in multi_entry:
+        if m in sheet.strip():
+            multi = True
+            break
+
+    ws = xw.Book(default_data).sheets['Data']
     obj = uc.Chrome()
-    ws = xw.Book(filename).sheets[sheet]
     obj.get(
         "https://evisa.immigration.gov.vn/vi_VN/web/guest/khai-thi-thuc-dien-tu/cap-thi-thuc-dien-tu?p_p_id=khaithithucdientu_WAR_eVisaportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_count=1&_khaithithucdientu_WAR_eVisaportlet_view=insert")
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ten").send_keys(ws.range("B2").value)
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ho_tt22").send_keys(ws.range("C2").value)
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngaySinh").send_keys(ws.range(
-        "D2").value.strftime("%d/%m/%Y"))
-    if ws.range("E2").value == "M":
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ten").send_keys(data[1])
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ho_tt22").send_keys(data[2])
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngaySinh").send_keys(datetime.datetime.strptime(
+        data[3], "%Y-%m-%d %H:%M:%S").strftime("%d/%m/%Y"))
+    if data[4] == "M":
         obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_gioiTinh-nam").click()
     else:
         obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_gioiTinh-nu").click()
 
-    Select(obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_quocTichHienTai")).select_by_value(ws.range(
-        "F2").value)
+    Select(obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_quocTichHienTai")).select_by_value(data[5])
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_tonGiao").send_keys("No")
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_email").send_keys("tkface02@gmail.com")
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_reEmail").send_keys("tkface02@gmail.com")
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_email").send_keys(ws.range("A3").value)
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_reEmail").send_keys(ws.range("A3").value)
 
-    if sheet == '1ThangNhieuLan (1TNL)' or sheet == '3ThangNhieuLan (3TNL)':
+    if multi:
         obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_passport_mutil_tt22").click()
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_fromDate").clear()
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_fromDate").send_keys(ws.range(
-        "G2").value.strftime("%d/%m/%Y"))
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_fromDate").send_keys(datetime.datetime.strptime(
+        data[6], "%Y-%m-%d %H:%M:%S").strftime("%d/%m/%Y"))
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_toDate").clear()
 
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_toDate").send_keys((ws.range("G2").value +
-        datetime.timedelta(days=stay)).strftime("%d/%m/%Y"))
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_soHoChieu").send_keys(ws.range("H2").value)
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_issueDateTt22").send_keys(
-        ws.range("I2").value.strftime("%d/%m/%Y"))
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngayCoGiaTri").send_keys(
-        ws.range("J2").value.strftime("%d/%m/%Y"))
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_toDate").send_keys((datetime.datetime.strptime(
+        data[6], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=stay)).strftime("%d/%m/%Y"))
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_soHoChieu").send_keys(data[7])
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_issueDateTt22").send_keys(datetime.datetime.strptime(
+        data[8], "%Y-%m-%d %H:%M:%S").strftime("%d/%m/%Y"))
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngayCoGiaTri").send_keys(datetime.datetime.strptime(
+        data[9], "%Y-%m-%d %H:%M:%S").strftime("%d/%m/%Y"))
 
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_diaChiLienHeTt22").send_keys(ws.range("K2").value)
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_diaChiLienHeTt22").send_keys(data[10])
 
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_soNgayTamTru").clear()
 
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_soNgayTamTru").send_keys(str(stay))
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngayNhapCanh").clear()
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngayNhapCanh").send_keys(ws.range(
-        "G2").value.strftime("%d/%m/%Y"))
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_ngayNhapCanh").send_keys(datetime.datetime.strptime(
+        data[6], "%Y-%m-%d %H:%M:%S").strftime("%d/%m/%Y"))
     Select(obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_cuaKhauNhapCanh")).select_by_value(ws.range(
-        "L2").value)
+        "B3").value)
     Select(obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_cuaKhauXuatCanh")).select_by_value(ws.range(
-        "M2").value)
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_diaChi").send_keys(ws.range("N2").value)
+        "C3").value)
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_diaChi").send_keys(ws.range("D3").value)
     Select(obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_maTtTT")).select_by_value(str(int(ws.range(
-        "O2").value)))
+        "E3").value)))
 
-    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_chiphidukien_tt22").send_keys(ws.range("P2").value)
+    obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_chiphidukien_tt22").send_keys("1000")
 
     obj.find_element(By.ID, "_khaithithucdientu_WAR_eVisaportlet_agree").click()
 
-    sleep(100000)
-    obj.quit()
--- sanji.py
+++ sanji.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -12,15 +13,14 @@ class App:
         self.root = root
         self.root.title("Ứng dụng Sanji by AHT-ASIA")
         self.root.geometry("800x600")  
-
+        self.sheet = None
         # Button Import Excel
-        import_button = tk.Button(root, text="Import Excel", command=self.load_excel)
+        import_button = tk.Button(root, text="Import Excel", height=2, width=20, command=self.load_excel)
         import_button.pack(pady=10)
         import_button.pack()
-        
+
         # Tạo Combobox
         self.combo_box = ttk.Combobox(root, values=list([]), width=50, state="readonly")
-       
         self.combo_box.bind("<<ComboboxSelected>>", self.on_combobox_select)
         self.combo_box.place(relx=0.5, rely=0.5, anchor="center")
         
@@ -40,19 +40,18 @@ class App:
         if file_path:
             try:
                 # Đọc tệp Excel
-                global xls, selected_option
                 xls = pd.ExcelFile(file_path)
                 self.urlExcel = xls
                 self.combo_box.pack(pady=10)
                 self.df = pd.read_excel(xls, sheet_name=xls.sheet_names[0])
                 sheet_names = pd.ExcelFile(file_path).sheet_names
                 selected_option = sheet_names[0]
+                self.sheet = selected_option
                 self.combo_box['values'] = sheet_names
                 self.combo_box.set(xls.sheet_names[0])
                 self.update_treeview(self.df)
             except Exception as e:
                 messagebox.showerror("Lỗi", f"Lỗi khi đọc tệp Excel: {str(e)}")
-
 
     def update_treeview(self, df):
         if self.tree:
@@ -64,11 +63,11 @@ class App:
 
     def on_combobox_select(self, event):
         selected_option = self.combo_box.get()
-        print(selected_option)
         self.df = pd.read_excel(self.urlExcel, sheet_name=selected_option)
         sheet_names = pd.ExcelFile(self.urlExcel).sheet_names
         self.combo_box['values'] = sheet_names
         self.update_treeview(self.df)
+        self.sheet = selected_option
 
     def center_window(self, width, height):
         screen_width = self.root.winfo_screenwidth()
@@ -107,10 +106,12 @@ class App:
     def on_treeview_select(self, event):
         selected_item = self.tree.selection()[0]
         data = self.tree.item(selected_item, "values")
-        print("Dữ liệu từ item được chọn:", data)
-        thread = threading.Thread(target=process, args=(xls, selected_option))
+        thread = threading.Thread(target=process, args=(self.sheet, data))
 
         thread.start()
+
+    def exit_tool(self):
+        exit(0)
 
 if __name__ == "__main__":
     # Lấy phiên bản của Pandas
@@ -119,5 +120,6 @@ if __name__ == "__main__":
     # In phiên bản Pandas
     print("Phiên bản Pandas:", pandas_version)
     root = tk.Tk()
+
     app = App(root)
     root.mainloop()
